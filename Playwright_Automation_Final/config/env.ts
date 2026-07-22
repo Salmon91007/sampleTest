d605ef9e60007fe8cc96064916b15fa6852b6f30
Salmon91007 <299171@ust.com>
2026-07-22 15:48:39 +0530

    Added

--- Playwright_Automation_Final/config/env.ts
+++ Playwright_Automation_Final/config/env.ts
@@ -5,7 +5,20 @@ dotenv.config();
 export const Environment = {
     BASE_URL: process.env.BASE_URL!,
 
-    USER_EMAIL: "alice@retailmart.com",
+    // USER_EMAIL: "alice@retailmart.com",
+    // PASSWORD: "Password@123!",
+
+    // CARD_NUMBER: "4024007153361885",
+    // CARD_EXPIRY: "12/30",
+    // CARD_CVV: "123",
+
+    // ADDRESS: "Hyderabad",
+    // CITY: "Hyderabad",
+    // STATE: "Telangana",
+    // ZIP: "50000",
+    // COUNTRY: "INDIA"
+
+    USER_EMAIL: process.env.USER_EMAIL,
     PASSWORD: "Password@123!",
 
     CARD_NUMBER: "4024007153361885",
